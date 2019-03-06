83f65804ffeca33640a557e2e9af1c2f62fd3b17
unknown <andrewlichao@gmail.com>
2019-03-06 07:14:24 -0800

    spelling fix

---
+++ index.js
@@ -0,0 +1,17 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var nb = document.getElementById("nb");
+
+// Get the offset position of the navbar
+var sticky = nb.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nb.classList.add("sticky")
+  } else {
+    nb.classList.remove("sticky");
+  }
+}
